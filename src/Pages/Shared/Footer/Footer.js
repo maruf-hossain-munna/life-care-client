c1b1af16264cf28ed14f8731557218822b15879d
maruf21hossain <marufhossainmunna21@gmail.com>
2022-11-08 00:22:17 +0600

    header and footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>This is footer</h2>
+            <footer className="footer footer-center p-4 mt-10 bg-base-300 text-base-content">
+                <div>
+                    <p>Copyright © 2022 - All right reserved by Life Care</p>
+                </div>
+            </footer>
         </div>
     );
 };
